1d02bad2338428df88353b9664e88e732367d44a
Amarnath AS <amarnathas2003@gmail.com>
2024-07-09 18:14:53 +0530

    fix: changes in services

--- frontend/src/services/user.services.js
+++ frontend/src/services/user.services.js
@@ -4,50 +4,50 @@ export const signin = async (credentials) => {
     try {
         const res = await API.post("/signin", credentials, { withCredentials: true });
         console.log(res)
-        return res.data;
+        return res?.data;
     } catch (error) {
-        console.log(error.response.data)
-        return error.response.data;
+        console.log(error.response?.data)
+        return error.response?.data;
     }
 }
 
 export const signup = async (credentials) => {
     try {
         const res = await API.post("/signup", credentials, { withCredentials: true });
-        return res.data;
+        return res?.data;
     } catch (error) {
-        console.log(error.response.data)
-        return error.response.data;
+        console.log(error.response?.data)
+        return error.response?.data;
     }
 }
 
 export const getProjects = async () => {
     try {
         const res = await API.get("/projects", { withCredentials: true });
-        return res.data;
+        return res?.data;
     } catch (error) {
-        console.log(error.response.data)
-        return error.response.data;
+        console.log(error.response?.data)
+        return error.response?.data;
     }
 }
 
 export const createProject = async (details) => {
     try {
         const res = await API.post("/projects/new", details, { withCredentials: true });
-        return res.data;
+        return res?.data;
     } catch (error) {
-        console.log(error.response.data)
-        return error.response.data;
+        console.log(error.response?.data)
+        return error.response?.data;
     }
 }
 
 export const getProject = async (id) => {
     try {
         const res = await API.get(`/projects/${id}`, { withCredentials: true });
-        return res.data;
+        return res?.data;
     } catch (error) {
-        console.log(error.response.data)
-        return error.response.data;
+        console.log(error.response?.data)
+        return error.response?.data;
     }
 }
 
@@ -56,8 +56,8 @@ export const editProject = async (id, project) => {
         const res = await API.patch(`/projects/update/${id}`, project, { withCredentials: true });
         return res.data;
     } catch (error) {
-        console.log(error.response.data);
-        return error.response.data;
+        console.log(error.response?.data);
+        return error.response?.data;
     }
 }
 
@@ -66,7 +66,7 @@ export const deleteProject = async (id) => {
         const res = await API.delete(`/projects/delete/${id}`, { withCredentials: true });
         return res.data;
     } catch (error) {
-        console.log(error.response.data)
-        return error.response.data;
+        console.log(error.response?.data)
+        return error.response?.data;
     }
 }
